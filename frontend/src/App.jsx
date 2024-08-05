896d078d339ce77745c5d46267896443d85c51a8
Ahmedbo9 <ahmed.benothman@live.fr>
2024-08-05 12:17:19 -0400

    frontend init commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@chakra-ui/react";
+const App = () => {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <p>This is a simple React app.</p>
+      <Button>Click me</Button>
+    </div>
+  );
+};
+
+export default App;

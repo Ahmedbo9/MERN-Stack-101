0427995e70c3fc45f8cf3311b70f9c5c0a4e20b1
Ahmedbo9 <ahmed.benothman@live.fr>
2024-08-05 22:36:48 -0400

    adding creation of product

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,5 +1,71 @@
+import { useState } from "react";
+import {
+  Container,
+  VStack,
+  Heading,
+  Box,
+  useColorModeValue,
+  Input,
+  Button,
+} from "@chakra-ui/react";
+import { useProductStore } from "../product";
 const CreatePage = () => {
-  return <div>CreatePafe</div>;
+  const [newProduct, setNewProduct] = useState({
+    name: "",
+    price: "",
+    image: "",
+  });
+
+  const { createProduct } = useProductStore();
+
+  const onSubmit = async () => {
+    const { success, message } = await createProduct(newProduct);
+    console.log(success, message);
+  };
+
+  return (
+    <Container maxW={"container.sm"} size={"2xl"} textAlign={"center"} mb={8}>
+      <VStack spacing={8}>
+        <Heading as={"h1"}>Create a Wish</Heading>
+        <Box
+          p={6}
+          rounded={"lg"}
+          shadow={"md"}
+          w={"full"}
+          bg={useColorModeValue("white", "grey.800")}
+        >
+          <VStack spacing={4}>
+            <Input
+              name="name"
+              value={newProduct.name}
+              placeholder="Product Name"
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, name: e.target.value })
+              }
+            />
+            <Input
+              name="price"
+              value={newProduct.price}
+              placeholder="Product price"
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, price: e.target.value })
+              }
+            />
+
+            <Input
+              name="image"
+              value={newProduct.image}
+              placeholder="Product Image"
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, image: e.target.value })
+              }
+            />
+            <Button onClick={onSubmit}>Create</Button>
+          </VStack>
+        </Box>
+      </VStack>
+    </Container>
+  );
 };
 
 export default CreatePage;

7a89377f1173f6f0b33df39eb1bc022f752cec37
Ahmedbo9 <ahmed.benothman@live.fr>
2024-08-09 18:30:21 -0400

    adding deployment config

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,15 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectToDatabase } from "./config/database.js";
 import ProductRoute from "./routes/product.route.js";
+import path from "path";
 
 dotenv.config();
 
 const port = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
 const app = express();
-app.listen( port , () => {
+app.listen(port, () => {
   connectToDatabase();
   console.log(`Server started on http://localhost:${port}`);
 });
@@ -18,5 +20,9 @@ app.use(express.json());
 
 app.use("/api/products", ProductRoute);
 
-
-
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  );
+}

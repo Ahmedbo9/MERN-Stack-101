7f1bfde24791d7619b1c758c18c7e9bbf08d6cb2
Ahmedbo9 <ahmed.benothman@live.fr>
2024-08-09 18:06:22 -0400

    project is complete

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -7,7 +7,20 @@ import {
   HStack,
   useColorModeValue,
   useToast,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  VStack,
+  Input,
+  useDisclosure,
 } from "@chakra-ui/react";
+
+import { useState } from "react";
 
 import { DeleteIcon, EditIcon } from "@chakra-ui/icons";
 import { useProductStore } from "../store/product.js";
@@ -15,9 +28,33 @@ import { useProductStore } from "../store/product.js";
 const ProductCard = ({ product }) => {
   const textColor = useColorModeValue("gray.800", "white");
   const bg = useColorModeValue("white", "gray.800");
-  const { deleteProduct } = useProductStore();
+  const { deleteProduct, updateProduct } = useProductStore();
 
   const toast = useToast();
+  const [newProduct, setNewProduct] = useState(product);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const handleUpdateProduct = async (productId) => {
+    const { success } = await updateProduct(productId, newProduct);
+    onClose();
+    if (!success) {
+      toast({
+        title: "Error",
+        description: "Product not updated",
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+      });
+    } else {
+      toast({
+        title: "Success",
+        description: "Product updated successfully",
+        status: "success",
+        duration: 3000,
+        isClosable: true,
+      });
+    }
+  };
 
   const handleDeleteProduct = async (productId) => {
     const { success, message } = await deleteProduct(productId);
@@ -65,7 +102,7 @@ const ProductCard = ({ product }) => {
         </Text>
 
         <HStack spacing={2}>
-          <IconButton icon={<EditIcon />} colorScheme="blue" />
+          <IconButton onClick={onOpen} icon={<EditIcon />} colorScheme="blue" />
           <IconButton
             icon={<DeleteIcon />}
             onClick={() => handleDeleteProduct(product._id)}
@@ -73,6 +110,57 @@ const ProductCard = ({ product }) => {
           />
         </HStack>
       </Box>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+
+        <ModalContent>
+          <ModalHeader>Update Product</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <VStack spacing={4}>
+              <Input
+                placeholder="Product Name"
+                name="name"
+                value={newProduct.name}
+                onChange={(e) =>
+                  setNewProduct({ ...newProduct, name: e.target.value })
+                }
+              />
+
+              <Input
+                placeholder="Price"
+                name="price"
+                type="number"
+                value={newProduct.price}
+                onChange={(e) =>
+                  setNewProduct({ ...newProduct, price: e.target.value })
+                }
+              />
+              <Input
+                placeholder="Image URL"
+                name="image"
+                value={newProduct.image}
+                onChange={(e) =>
+                  setNewProduct({ ...newProduct, image: e.target.value })
+                }
+              />
+            </VStack>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button
+              colorScheme="blue"
+              mr={3}
+              onClick={() => handleUpdateProduct(product._id, setNewProduct)}
+            >
+              Update
+            </Button>
+            <Button variant="ghost" onClick={onClose}>
+              Cancel
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 };
--- frontend/src/store/product.js
+++ frontend/src/store/product.js
@@ -39,4 +39,24 @@ export const useProductStore = create((set) => ({
     }));
     return { success: true, message: "Product deleted successfully" };
   },
+
+  updateProduct: async (id, updatedProduct) => {
+    const res = await fetch(`/api/products/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedProduct),
+    });
+    const data = await res.json();
+    if (!data.success) {
+      return { success: false, message: data.message };
+    }
+    set((state) => ({
+      products: state.products.map((product) =>
+        product._id === id ? data.data : product
+      ),
+    }));
+    return { success: true, message: "Product updated successfully" };
+  },
 }));
